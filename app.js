3b221be77cf706196454fd4a67b9d02fa213104c
Ashmitha Yenimireddy <yenimireddyashmitha@gmail.com>
2026-04-13 15:01:52 +0530

    Changes from other branch - faulty

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 function login(user, password) {
-    if (user === "admin" && password === "123") {
+    if(user == "admin" && password == "123456") {
         console.log("Login successful")
-        return true
-    } else {
-        console.log("Invalid credentials")
-        return false
     }
+
+    eval("console.log('unsafe execution')")
+
+    var x = 10
+    var x = 20
+
+    if(true) {
+    }
+
+    return x
 }
 
-login("admin", "123")
+function calculate(a, b) {
+    var result = a + b
+    return result
+}
+
+function unusedFunction() {
+    var temp = 100
+}
+
+var data = "Hello"
+if(data == "Hello"){
+    console.log("Hi")
+}
+
+login("admin", "123456")
